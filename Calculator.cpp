8c427a4440e1e09feb6e610ce5dcf471f99f2e23
Ezz Eldin Ahmed Tabaga <ezztabaga@gmail.com>
2026-03-10 21:28:55 +0200

    add functions to calculator

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
+
+double Addition(double a, double b){
+    return a+b;
+}
+
+double Subtraction(double a, double b){
+    return a-b;
+}
+
+double Multiplication(double a, double b){
+    return a*b;
+}
+
+double Division(double a, double b){
+    if (b != 0){
+        return a/b;
+    } else {
+        cout << "Error: Division by zero is not allowed." << endl;
+        return 0;
+    }
+}
+
+
 int main(){
     double num1;
     double num2;
@@ -16,35 +39,29 @@ int main(){
         cin>>num1;
         cout<<"Enter second number: ";
         cin>>num2;
-            switch(op){
+        switch(op){
         case '+':
         cout<<"==Addition=="<<endl;
-        cout<<"Result: "<<num1+num2<<endl;
+        cout<<"Result: "<<Addition(num1, num2)<<endl;
         cout<<"============"<<endl;
         break;
         case '-':
         cout<<"==Subtraction=="<<endl;
-        cout<<"Result: "<<num1-num2<<endl;
+        cout<<"Result: "<<Subtraction(num1, num2)<<endl;
         cout<<"================="<<endl;
         break;
         case '*':
         cout<<"==Multiplication=="<<endl;
-        cout<<"Result: "<<num1*num2<<endl;
+        cout<<"Result: "<<Multiplication(num1, num2)<<endl;
         cout<<"=================="<<endl;
         break;
         case '/':
-        if (num2!=0){
-            cout<<"==Division=="<<endl;
-            cout<<"Result: "<<num1/num2<<endl;
-            cout<<"============"<<endl;
-        }
-        else{
-            cout<<"Error: Division by zero is not allowed.";
-        }
+        cout<<"==Division=="<<endl;
+        cout<<"Result: "<<Division(num1, num2)<<endl;
+        cout<<"============"<<endl;
         break;
         default:
-        cout<<"Please choose a valid operator from the options provided.";
-
+        cout<<"Invalid operator. Please try again."<<endl;
     }
 
         cout<<endl;
